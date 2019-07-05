2b44abec350854a21dd6992dbd45c3cfc51e5ba9
tim-stark-m45 <tim.stark_74@yahoo.com>
2019-07-05 22:26:43 +0400

    Second Commit

---
+++ WpfAppCommands1/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppCommands1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel vm = new MainWindowViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = vm;
+        }
+    }
+
+    public class MainWindowViewModel : ObservableObject
+    {
+        private string text = "Hello!";
+        public string Text { get => text; set => Set(ref text, value); }
+
+        private RelayCommand testCommand;
+        public RelayCommand TestCommand
+        { get => testCommand ?? (testCommand = new RelayCommand(
+            param =>
+            {
+                Text = "Test";
+            }
+            ));
+        }
+        public MainWindowViewModel()
+        {
+            ///TestCommand = new RelayCommand(param => Text = "Test");
+        }
+
+        //public RelayCommand TestCommand { get; set; }
+    }
+}
---
+++ WpfAppCommands1/ObservableObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppCommands1
+{
+    public class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void Set<T>(ref T field, T value, [CallerMemberName]string prop = "")
+        {
+            field = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
